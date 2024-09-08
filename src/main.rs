f2f41d6e9d5a69406a3c945663fa79e8ceeebbc2
daite <blueskykind02@yahoo.co.jp>
2024-09-08 21:26:01 +0900

    [Fixed] angel path

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::process::Command;
 use std::io::{self, BufRead};
 use std::env;
+use which::which; // Add the 'which' crate in Cargo.toml
+use regex::Regex;
 
 fn main() -> io::Result<()> {
     // Get the keyword from command-line arguments
@@ -11,24 +13,38 @@ fn main() -> io::Result<()> {
     }
     let keyword = &args[1];
 
-    // Run the Go program with the -l jp argument and the search keyword
-    let golang_output = Command::new("/usr/local/bin/angel")
+    // Find the 'angel' program in the $PATH
+    let angel_path = match which("angel") {
+        Ok(path) => {
+            println!("[+] Found 'angel' at: {}", path.display());
+            path
+        },
+        Err(_) => {
+            eprintln!("[-] 'angel' program not found in $PATH");
+            std::process::exit(1);
+        }
+    };
+
+    // Run the 'angel' program with the -l jp argument and the search keyword
+    let golang_output = Command::new(angel_path)
         .arg("-l")
         .arg("jp")
         .arg(keyword)
         .output()
-        .expect("Failed to run Go program");
+        .expect("[-] Failed to run 'angel' program");
+
     // Capture the output and convert it to a string
     let output = String::from_utf8(golang_output.stdout).expect("Failed to parse output");
 
     // Read the output line by line
     let reader = io::BufReader::new(output.as_bytes());
 
+    let re = Regex::new(r"magnet:\?xt=urn:btih:[a-zA-Z0-9]{40}").expect("Failed to compile regex");
+
     for line in reader.lines() {
         let line = line?;
-        
+
         // Extract magnet link using a regular expression
-        let re = regex::Regex::new(r"magnet:\?xt=urn:btih:[a-zA-Z0-9]{40}").expect("Failed to compile regex");
         if let Some(magnet_link) = re.find(&line) {
             // Open the magnet link with qBittorrent
             Command::new("open")
@@ -39,6 +55,5 @@ fn main() -> io::Result<()> {
                 .expect("Failed to open qBittorrent with magnet link");
         }
     }
-
     Ok(())
 }
